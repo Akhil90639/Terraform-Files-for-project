18a0f6c4cdf9f3437b7d7d854c333f4e148068f4
Akhil <111115830+Akhil90639@users.noreply.github.com>
2023-02-06 23:08:52 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -3,5 +3,5 @@
 
 provider "aws" {
   region     = "us-east-1"
-  access_key = ""
-  secret_key = ""
+  access_key = "AKIAXM35LSXSMZXH2S4O"
+  secret_key = "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"
